38a1d85e6f860afa602ffd5a99ab0beab39ebe26
Ilya Boiko <boiko.ilya.evg@gmail.com>
2024-08-23 16:40:44 +0500

    4.3.13

--- Program.cs
+++ Program.cs
@@ -10,6 +10,22 @@ namespace SF_CS_004
     {
         static void Main(string[] args)
         {
+            var Array = new int[] { 5, 6, 9, 1, 2, 3, 4 };
+            var Result = 0;
+
+            Console.Write("Текущий массив: ");
+            for (int i = 0; i < Array.Length; i++)
+            {
+                Console.Write((i > 0 ? ", " : "") + Array[i]);
+            }
+
+            foreach (var Item in Array)
+            {
+                Result += Item;
+            }
+
+            Console.WriteLine("\nСумма всех элементов массива: " + Result);
+            Console.ReadKey();
         }
     }
 }
